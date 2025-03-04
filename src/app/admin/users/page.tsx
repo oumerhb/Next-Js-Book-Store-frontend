dea04bf61ac523065eaab92cb839c886f56b6ecc
Oumer Habib Badebo <oumerhb@gmail.com>
2025-03-04 23:10:36 +0300

    update responsiveness

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -9,12 +9,12 @@ export default function ManageUsers() {
         <input
           type="text"
           placeholder="Search users..."
-          className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white text-gray-700 w-full max-w-md"
+          className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white text-gray-700 w-full sm:max-w-lg"
         />
       </div>
 
       {/* Users Table */}
-      <div className="bg-white p-6 rounded-lg shadow-md">
+      <div className="bg-white p-6 rounded-lg shadow-md ">
         <h3 className="text-lg font-semibold text-gray-700 mb-4">User List</h3>
         <table className="w-full">
           <thead>
